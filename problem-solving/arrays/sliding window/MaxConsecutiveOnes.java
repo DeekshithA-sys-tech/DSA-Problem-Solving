60ac06bbb2c24274c7f73753d513e02f5fcaa178
Deekshith A <deekshith.a561203@gmail.com>
2026-07-04 22:27:21 +0530

    Added MaxConsecutiveOnes.java

---
+++ problem-solving/arrays/sliding window/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+public class MaxConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0; // reset when encountering a 0
+            }
+        }
+
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1,1,1,1, 0, 1, 1, 1};
+        System.out.println("Max Consecutive Ones: " + findMaxConsecutiveOnes(nums));
+    }
+}
