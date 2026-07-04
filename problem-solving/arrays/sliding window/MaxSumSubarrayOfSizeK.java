bbba1541d618e4ea7bb358f29a59c17c61ba0871
Deekshith A <deekshith.a561203@gmail.com>
2026-07-04 22:13:17 +0530

    Sliding Window solution in Java to find the maximum sum subarray of size k

---
+++ problem-solving/arrays/sliding window/MaxSumSubarrayOfSizeK.java
@@ -0,0 +1,33 @@
+public class MaxSumSubarrayOfSizeK {
+    public static int maxSumSubarray(int[] arr, int k) {
+        int n = arr.length;
+        if (n < k) {
+            throw new IllegalArgumentException("Array size must be >= k");
+        }
+
+        // Compute sum of first window
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i] - arr[i - k]; 
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+
+        int result = maxSumSubarray(arr, k);
+        System.out.println("Maximum sum of subarray of size " + k + " is: " + result);
+    }
+
+}
