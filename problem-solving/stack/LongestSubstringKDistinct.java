fb4d2fbf810d18e278b64d60f90f0692eb30e913
Deekshith A <deekshith.a561203@gmail.com>
2026-07-10 22:05:45 +0530

    Solution : longest substring that contains at most k distinct characters.

---
+++ problem-solving/stack/LongestSubstringKDistinct.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class LongestSubstringKDistinct {
+    public static int lengthOfLongestSubstringKDistinct(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+            while (map.size() > k) {
+                char d = s.charAt(left++);
+                map.put(d, map.get(d) - 1);
+                if (map.get(d) == 0) map.remove(d);
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstringKDistinct("eceba", 2)); // Output: 3 ("ece")
+    }
+}
