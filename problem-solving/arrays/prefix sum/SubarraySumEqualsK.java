ddbfda993f009df7193836745d96b9726102e867
Deekshith A <deekshith.a561203@gmail.com>
2026-07-13 22:15:03 +0530

    Solution : the number of subarrays whose sum equals k

---
+++ problem-solving/arrays/prefix sum/SubarraySumEqualsK.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class SubarraySumEqualsK {
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // base case
+        int sum = 0, count = 0;
+
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, -2, 5};
+        int k = 5;
+        System.out.println(subarraySum(nums, k)); 
+    }
+}
